ed1d858be35d81d44774db548c51386d5a70d453
Nickolay Yegorov <nickolay.yegorov@gmail.com>
2014-11-07 13:16:17 +0300

    patches: removed black strips with padding mode; autorotation

--- lib/paperclip_processors/ffmpeg.rb
+++ lib/paperclip_processors/ffmpeg.rb
@@ -1,7 +1,7 @@
 module Paperclip
   class Ffmpeg < Processor
     attr_accessor :geometry, :format, :whiny, :convert_options
-    ConvertorNotFoundError = Class.new(StandardError)
+
     # Creates a Video object set to work on the +file+ given. It
     # will attempt to transcode the video into one defined by +target_geometry+
     # which is a "WxH"-style string. +format+ should be specified.
@@ -21,22 +21,21 @@ module Paperclip
           @convert_options[:output].reverse_merge! options[:convert_options][:output]
         end
       end
-
-      @geometry         = options[:geometry]
-      @file             = file
-      @keep_aspect      = !@geometry.nil? && @geometry[-1,1] != '!'
-      @pad_only         = @keep_aspect    && @geometry[-1,1] == '#'
-      @enlarge_only     = @keep_aspect    && @geometry[-1,1] == '<'
-      @shrink_only      = @keep_aspect    && @geometry[-1,1] == '>'
-      @keep_aspect_mode = options[:keep_aspect_mode] == 'save_height' ? :save_height : :save_width
-      @whiny            = options[:whiny].nil? ? true : options[:whiny]
-      @format           = options[:format]
-      @time             = options[:time].nil? ? 3 : options[:time]
-      @current_format   = File.extname(@file.path)
-      @basename         = File.basename(@file.path, @current_format)
-      @meta             = identify
-      @pad_color        = options[:pad_color].nil? ? "black" : options[:pad_color]
-      @auto_rotate      = options[:auto_rotate].nil? ? false : options[:auto_rotate]
+      
+      @geometry        = options[:geometry]
+      @file            = file
+      @keep_aspect     = !@geometry.nil? && @geometry[-1,1] != '!'
+      @pad_only        = @keep_aspect    && @geometry[-1,1] == '#'
+      @enlarge_only    = @keep_aspect    && @geometry[-1,1] == '<'
+      @shrink_only     = @keep_aspect    && @geometry[-1,1] == '>'
+      @whiny           = options[:whiny].nil? ? true : options[:whiny]
+      @format          = options[:format]
+      @time            = options[:time].nil? ? 3 : options[:time]
+      @current_format  = File.extname(@file.path)
+      @basename        = File.basename(@file.path, @current_format)
+      @meta            = identify
+      @pad_color       = options[:pad_color].nil? ? "black" : options[:pad_color]
+      @auto_rotate     = options[:auto_rotate].nil? ? false : options[:auto_rotate]
       attachment.instance_write(:meta, @meta)
     end
     # Performs the transcoding of the +file+ into a thumbnail/video. Returns the Tempfile
@@ -48,40 +47,10 @@ module Paperclip
       dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
       Ffmpeg.log("Destination File Built") if @whiny
       dst.binmode
-
+      
       parameters = []
 
-      # If file has rotation, rotate it back to horizontal
-      if @auto_rotate && !@meta[:rotate].nil?
-        Ffmpeg.log("Adding rotation #{@meta[:rotate]}") if @whiny
-        case @meta[:rotate]
-        when 90
-          @convert_options[:output][:vf] = 'transpose=1'
-        when 180
-          @convert_options[:output][:vf] = "'vflip, hflip'"
-        when 270
-          @convert_options[:output][:vf] = 'transpose=2'
-        end
-        if [90, 100, 270].include? @meta[:rotate]
-          rotated = true
-        end
-      end
-
       Ffmpeg.log("Adding Geometry") if @whiny
-
-      # If file has rotation, rotate it back to horizontal
-      if @auto_rotate && !@meta[:rotate].nil?
-        Ffmpeg.log("Adding rotation #{@meta[:rotate]}") if @whiny
-        case @meta[:rotate]
-        when 90
-          @convert_options[:output][:vf] = 'transpose=1'
-        when 180
-          @convert_options[:output][:vf] = 'vflip,hflip'
-        when 270
-          @convert_options[:output][:vf] = 'transpose=2'
-        end
-      end
-
       # Add geometry
       if @geometry
         Ffmpeg.log("Extracting Target Dimensions") if @whiny
@@ -94,6 +63,15 @@ module Paperclip
         unless @meta[:size].nil?
           Ffmpeg.log("Target Size is Available") if @whiny
           current_width, current_height = @meta[:size].split('x')
+
+
+        # if auto_portrait_landspace
+        if @auto_portrait_landspace && ( \
+            ((current_width < current_height) && (target_width > target_height)) || \
+            ((current_width > current_height) && (target_width < target_height)) )
+          target_width, target_height = target_height, target_width     
+        end
+
           # Current width and height
           if @keep_aspect
             Ffmpeg.log("Keeping Aspect Ratio") if @whiny
@@ -113,7 +91,8 @@ module Paperclip
               Ffmpeg.log("Shrink Only") if @whiny
               if current_width.to_i > target_width.to_i
                 # Keep aspect ratio
-                width, height = calculate_output_file_dimensions(target_width, target_height)
+                width = target_width.to_i
+                height = (width.to_f / (@meta[:aspect].to_f)).to_i
                 @convert_options[:output][:s] = "#{width.to_i/2*2}x#{height.to_i/2*2}"
                 Ffmpeg.log("Convert Options: #{@convert_options[:output][:s]}") if @whiny
               else
@@ -122,23 +101,38 @@ module Paperclip
               end
             elsif @pad_only
               Ffmpeg.log("Pad Only") if @whiny
-              # Keep aspect ratio
-              width, height = calculate_output_file_dimensions(target_width, target_height)
-              # We should add half the delta as a padding offset Y
-              pad_y = (target_height.to_f - height.to_f) / 2
+
               # There could be options already set
-              @convert_options[:output][:vf][/\z/] = ',' if @convert_options[:output][:vf]
+              @convert_options[:output][:vf][/\A/] = ',' if @convert_options[:output][:vf]
               @convert_options[:output][:vf] ||= ''
-              if pad_y > 0 && rotated.nil?
-                @convert_options[:output][:vf][/\z/] = "scale=#{width}:-1,pad=#{width.to_i}:#{target_height.to_i}:0:#{pad_y}:#@pad_color"
+
+              width  = target_width.to_i
+              height = target_height.to_i
+              
+              if width > height
+                xwidth = [(height.to_f * @meta[:aspect].to_f).to_i, width].max
+                @convert_options[:output][:vf][/\A/] = "scale=#{xwidth}:-1,crop=#{width.to_i}:#{height.to_i}"
               else
-                @convert_options[:output][:vf][/\z/] = "scale=#{width}:-1,crop=#{width.to_i}:#{width.to_i}"
+                xheight = [(width.to_f / @meta[:aspect].to_f).to_i, height].max
+                @convert_options[:output][:vf][/\A/] = "scale=-1:#{xheight},crop=#{width.to_i}:#{height.to_i}"
               end
+              # # Keep aspect ratio
+              # width = target_width.to_i
+              # height = (width.to_f / (@meta[:aspect].to_f)).to_i
+              # # We should add half the delta as a padding offset Y
+              # pad_y = (target_height.to_f - height.to_f) / 2
+
+              # if pad_y > 0
+              #   @convert_options[:output][:vf][/\A/] = "scale=#{width}:-1,pad=#{width.to_i}:#{target_height.to_i}:0:#{pad_y}:#@pad_color"
+              # else
+              #   @convert_options[:output][:vf][/\A/] = "scale=#{width}:-1,crop=#{width.to_i}:#{height.to_i}"
+              # end
               Ffmpeg.log("Convert Options: #{@convert_options[:output][:s]}") if @whiny
             else
               Ffmpeg.log("Resize") if @whiny
               # Keep aspect ratio
-              width, height = calculate_output_file_dimensions(target_width, target_height)
+              width = target_width.to_i
+              height = (width.to_f / (@meta[:aspect].to_f)).to_i
               @convert_options[:output][:s] = "#{width.to_i/2*2}x#{height.to_i/2*2}"
               Ffmpeg.log("Convert Options: #{@convert_options[:output][:s]}") if @whiny
             end
@@ -148,6 +142,22 @@ module Paperclip
             @convert_options[:output][:s] = "#{target_width.to_i/2*2}x#{target_height.to_i/2*2}"
             Ffmpeg.log("Convert Options: #{@convert_options[:output][:s]}") if @whiny
           end
+        end
+      end
+
+      # If file has rotation, rotate it back to horizontal
+      if @auto_rotate && !@meta[:rotate].nil?
+        @convert_options[:output][:vf] ||= ''
+        comma = @convert_options[:output][:vf].blank? ? '' : ','
+
+        Ffmpeg.log("Adding rotation #{@meta[:rotate]}") if @whiny
+        case @meta[:rotate]
+        when 90
+          @convert_options[:output][:vf] += comma + 'transpose=1'
+        when 180
+          @convert_options[:output][:vf] += comma + 'vflip,hflip'
+        when 270
+          @convert_options[:output][:vf] += comma + 'transpose=2'
         end
       end
 
@@ -192,23 +202,7 @@ module Paperclip
 
       dst
     end
-
-    def calculate_output_file_dimensions width, height
-      if @keep_aspect_mode == :save_height
-        [(height.to_f * @meta[:aspect].to_f).to_i, height.to_i]
-      else
-        width = width.to_i
-        height = if @meta[:rotate].in? [90, 270]
-          # target calculations are based on the meta info of
-          # the original (un-rotated) image, so we need to
-          # invert the aspect ratio when we're transposing the image
-          width * @meta[:aspect]
-        else
-          (width.to_f / (@meta[:aspect].to_f)).to_i
-        end
-      end
-    end
-
+    
     def identify
       meta = {}
       av_lib_version = Ffmpeg.detect_ffprobe_or_avprobe
@@ -231,8 +225,8 @@ module Paperclip
         if line =~ /Duration:(\s.?(\d*):(\d*):(\d*\.\d*))/
           meta[:length] = $2.to_s + ":" + $3.to_s + ":" + $4.to_s
         end
-        if line =~ /rotate\s*:\s(\d*)/
-          meta[:rotate] = $1.to_i
+        if line =~ /rotate\s*:\s(\d*)/ 
+          meta[:rotate] = $1.to_i 
         end
       end
       Paperclip.log("[ffmpeg] Command Success") if @whiny
@@ -246,27 +240,31 @@ module Paperclip
 
     def self.detect_ffmpeg_or_avconv
       # Check whether ffmpeg or avconv is installed
-      if Ffmpeg.detect_command("ffmpeg")
+      result = Ffmpeg.detect_command("ffmpeg")
+      Ffmpeg.log("Result of command: #{result}") if @whiny
+      if result == true
         Ffmpeg.log("Result of command: #{"ffmpeg"}") if @whiny
           return "ffmpeg"
-      elsif Ffmpeg.detect_command("avconv")
+      elsif result == false
         Ffmpeg.log("Result of command: #{"avconv"}") if @whiny
           return "avconv"
       else
-        raise ConvertorNotFoundError, "no video conversion library detected. Please install ffmpeg or avconv."
+        return "Error: no video conversion library detected. Please install ffmpeg or avconv."
       end
     end
 
     def self.detect_ffprobe_or_avprobe
       # Check whether ffprobe or avprobe is installed
-      if Ffmpeg.detect_command("ffprobe")
+      result = Ffmpeg.detect_command("ffprobe")
+      Ffmpeg.log("Result of command: #{result}") if @whiny
+      if result == true
         Ffmpeg.log("Result of command: #{"ffprobe"}") if @whiny
         return "ffprobe"
-      elsif Ffmpeg.detect_command("avprobe")
+      elsif result == false
         Ffmpeg.log("Result of command: #{"avprobe"}") if @whiny
         return "avprobe"
       else
-        raise ConvertorNotFoundError, "no video conversion library detected. Please install ffmpeg or avconv."
+        return "Error: no video conversion library detected. Please install ffmpeg or avconv."
       end
     end
 
@@ -280,10 +278,12 @@ module Paperclip
           return true
         when /false/
           return false
+        else
+          return nil
       end
     end
   end
-
+  
   class Attachment
     def meta
       instance_read(:meta)
